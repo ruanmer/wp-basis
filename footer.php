a963d8674199da23ece5862414702d0efbbe2655
Ruan Mer <ruanrm@gmail.com>
2012-06-03 12:56:35 -0300

    fechando div corretamente

--- footer.php
+++ footer.php
@@ -1,9 +1,10 @@
-</div><!-- /#main -->
+	</div><!-- /#main -->
 
-<div id="footer">
-	<a href="<?php echo THEME_AUTHOR_URL; ?>" id="" title="<?php echo THEME_AUTHOR; ?>" target="_blank"><?php echo THEME_AUTHOR; ?></a>
-</div><!-- /#footer -->
+	<div id="footer">
+		<a href="<?php echo THEME_AUTHOR_URL; ?>" id="" title="<?php echo THEME_AUTHOR; ?>" target="_blank"><?php echo THEME_AUTHOR; ?></a>
+	</div><!-- /#footer -->
 
+</div><!-- /#container -->
 <?php
 	wp_footer();
 ?>
